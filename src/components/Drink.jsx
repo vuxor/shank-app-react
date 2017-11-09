24a070ce3e9b2951dc46ce8b54bbc8b7721475b1
NemanjaDjo <ndjorovic1@gmai.com>
2017-11-09 14:25:13 +0100

    KomponentaDrink

---
+++ src/components/Drink.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Drink extends React.Component{
+	render(){
+		return(
+			<div>
+				<p>
+					<img />
+					<span>pivo</span>
+					<span>150</span>
+				</p>
+			</div>
+
+		)
+	}
+}
