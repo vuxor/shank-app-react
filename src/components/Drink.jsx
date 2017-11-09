f95b19c6e1018fc6e9c31870fd6166e60a757c03
NemanjaDjo <ndjorovic1@gmai.com>
2017-11-09 15:22:46 +0100

    Izmena

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -5,9 +5,9 @@ export class Drink extends React.Component{
 		return(
 			<div>
 				<p>
-					<img />
-					<span>pivo</span>
-					<span>150</span>
+					<img src={this.props.slika} alt={this.props.naziv}/>
+					<span>{this.props.naziv}</span>
+					<span>{this.props.cena}</span>
 				</p>
 			</div>
 
