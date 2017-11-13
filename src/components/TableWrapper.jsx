a8ac666c875b4db73e6d4524933f9e23fd904798
ognjen91 <31930445+ognjen91@users.noreply.github.com>
2017-11-13 15:06:34 +0100

    Add TableWrapper
    table wrapper fix

--- src/components/TableWrapper.jsx
+++ src/components/TableWrapper.jsx
@@ -12,31 +12,18 @@ export class TableWrapper extends React.Component {
              width="100%"
              height="100vh"
              border = "5px solid green">
-       
-       
-       
 
                 <Table />
                 <Table />
                 <Table />
                 <Table />
-  
-       
-       
-       
+ 
        </Element>
    
    
    
-   )
-  }
-    
-    
-    
-    
-    
-    
-    
+        )
+          }
     
     
     
@@ -53,33 +40,6 @@ export class TableWrapper extends React.Component {
 
 
 
-
-//
-//export class Table extends React.Component {
-//
-//  render() {
-//    return (
-//     <div>
-//     	<Element id="table-1"
-//     		fontSize="23"
-//     		fontSizeOnHover="27"
-//     		width="100px"
-//     		height="100px"
-//     		background="#999"
-//     		backgroundOnHover="#000"
-//  			color="#000"
-//     		colorOnHover="#999"
-//     		cursorOnHover="pointer"
-//  			padding={10}>
-//  			Table 1
-//		</Element>
-//      </div>
-//    );
-//  }
-//}
-//    
-//
-//
 
 
 
